f6307b40d88552ee49939c591c0c467febfd4bb5
Donnacha Forde <donnachaforde@users.noreply.github.com>
2025-09-01 19:42:41 +0100

    Update TestArgs.cxx
    Added more tests to validate pre-parsing status; Tests expected values and keywords present; Renamed early tests to better specify the nature/purpose of the tes.

--- tests/TestArgs.cxx
+++ tests/TestArgs.cxx
@@ -2,21 +2,21 @@
 #include <espresso.hxx>
 using namespace espresso;
 
-TEST(ArgsTest, GetProgramName)
+TEST(ArgsTest, GetProgramNameReturnsExpectedName)
 {
     const string strProgramName = "myprog";
     Args args(0, nullptr, strProgramName, "desc", "0.1", "author", "2025", "@handle");
     EXPECT_EQ(args.getProgramName(), strProgramName);
 }
 
-TEST(ArgsTest, GetProgramDescription) 
+TEST(ArgsTest, GetProgramDescriptionReturnsExpectedDescription) 
 {
     const string strProgramDescription = "desc.";
     Args args(0, nullptr, "test", strProgramDescription, "0.1", "author", "2025", "@handle");
     EXPECT_EQ(args.getProgramDescription(), strProgramDescription);
 }
 
-TEST(ArgsTest, GetVersion) 
+TEST(ArgsTest, GetVersionReturnsExpectedVersion) 
 {
     const string strVersion = "1.2.3";
     Args args(0, nullptr, "test", "desc", strVersion, "author", "2025", "@handle");
@@ -24,7 +24,7 @@ TEST(ArgsTest, GetVersion)
 }
 
 
-TEST(ArgsTest, GetYearAndOwner)
+TEST(ArgsTest, GetCopyrightDetailsReturnsExpectedCopyrightYearAndOwner)
 {
     const string strYear = "2025";
     const string strCopyrightOwner = "John Smith";
@@ -34,7 +34,7 @@ TEST(ArgsTest, GetYearAndOwner)
     EXPECT_EQ(args.getCopyrightNotice(), strCopyrightMessage);
 }
 
-TEST(ArgsTest, GeBugReportingHandle)
+TEST(ArgsTest, GeBugReportingInstructionsReturnsExpectedContact)
 {
     const string strHandle = "@janedoe";
     string strMessage = "Report bugs to " + strHandle + ".";
@@ -42,19 +42,66 @@ TEST(ArgsTest, GeBugReportingHandle)
     EXPECT_EQ(args.getBugReportingInstructions(), strMessage);
 }
 
-// TEST(ArgsTest, DefaultHelpPresent) 
-// {
-//     Args args(0, nullptr, "test", "desc", "0.1", "author", "2025", "@handle");
-//     args.addDefaults();
 
-// 	ArgManager argMgr = ArgManagerFactory::createInstance();
-// 	int nRetVal = argMgr.parseAndProcessArgs(args);
-// 	if (nRetVal != 0)
-// 	{
-// 		::exit(0);
-// 	}    
+TEST(ArgsTest, TargetRequiredNotPresentBeforehand) 
+{
+    Args args(0, nullptr, "test", "desc", "0.1", "author", "2025", "@handle");
+    EXPECT_FALSE(args.isTargetPresent());
+}
 
-//     EXPECT_TRUE(args.isPresent("help"));
-// }
+TEST(ArgsTest, TargetShouldBeAnEmptyStringBeforehand) 
+{
+    Args args(0, nullptr, "test", "desc", "0.1", "author", "2025", "@handle");
+    EXPECT_EQ(args.getTarget(), "");
+}
+
+TEST(ArgsTest, TargetPresentShouldBeFalseBeforeParsing) 
+{
+    Args args(0, nullptr, "test", "desc", "0.1", "author", "2025", "@handle");
+    EXPECT_FALSE(args.isTargetRequired());
+}
+
+TEST(ArgsTest, TargetShouldBeAnEmptyStringBeforeParsing) 
+{
+    Args args(0, nullptr, "test", "desc", "0.1", "author", "2025", "@handle");
+    EXPECT_EQ(args.getTarget(), "");
+}
+
+TEST(ArgsTest, InvalidOptionShouldBeEmptyStringBeforeParsing) 
+{
+    Args args(0, nullptr, "test", "desc", "0.1", "author", "2025", "@handle");
+    EXPECT_EQ(args.getInvalidOption(), "");
+}
+
+TEST(ArgsTestPostSetup, GetUsageReturnsExpectedKeywords)
+{
+    Args args(0, nullptr, "myprog", "A test program", "1.0", "John Smith", "2025", "bugs@example.com");
+    args.add("--input", Arg::type_t::STRING, true, "Input file");
+    args.add("--output", Arg::type_t::STRING, false, "Output file");
+    args.addDefaults();
+
+    std::string usage = args.getUsage();
+
+    // Check that usage contains expected switches and program name
+    EXPECT_NE(usage.find("myprog"), std::string::npos);
+    EXPECT_NE(usage.find("Usage:"), std::string::npos);
+    EXPECT_NE(usage.find("Usage"), std::string::npos);
+}
+
+TEST(ArgsTestPostSetup, GetUsageReturnsRequiredArgs)
+{
+    Args args(0, nullptr, "myprog", "A test program", "1.0", "John Smith", "2025", "bugs@example.com");
+    args.add("--required", Arg::type_t::STRING, true, "Input file");
+    args.add("--optional", Arg::type_t::STRING, false, "Output file");
+
+    std::string usage = args.getUsage();
+
+    EXPECT_NE(usage.find("required"), std::string::npos);
+    EXPECT_NE(usage.find("optional"), std::string::npos);
+}
+
+
+
+
 
 
